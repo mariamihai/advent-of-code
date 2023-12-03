ecc2a88012958e9eb9504a845f60cd98a19a711d
maria <maria.ic.mihai@gmail.com>
2023-12-03 21:35:23 +0100

    day 3 - part 1

---
+++ 2023/day3/day3_test.go
@@ -0,0 +1,52 @@
+package day3
+
+import (
+	"testing"
+)
+
+func TestProblem1(t *testing.T) {
+	tests := []struct {
+		description string
+		filename    string
+		expected    int
+	}{
+		{
+			description: "Should validate PROBLEM 1 against provided EXAMPLE",
+			filename:    "example1.txt",
+			expected:    4361,
+		},
+		{
+			description: "Should validate PROBLEM 1 against provided INPUT",
+			filename:    "input1.txt",
+			expected:    519444,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			if actual := Problem1(tt.filename); actual != tt.expected {
+				t.Errorf("Problem1() = %v, want %v", actual, tt.expected)
+			}
+		})
+	}
+}
+
+//func TestProblem2(t *testing.T) {
+//	tests := []struct {
+//		description string
+//		filename    string
+//		expected    int
+//	}{
+//		{
+//			description: "Should validate PROBLEM 2 against provided EXAMPLE",
+//			filename:    "example1.txt",
+//			expected:    467835,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.description, func(t *testing.T) {
+//			if actual := Problem2(tt.filename); actual != tt.expected {
+//				t.Errorf("Problem2() = %v, want %v", actual, tt.expected)
+//			}
+//		})
+//	}
+//}
