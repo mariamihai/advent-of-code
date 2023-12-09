1c8ab269e3b99f4d7edf954475a97b26e7085d9b
maria <maria.ic.mihai@gmail.com>
2023-12-09 12:22:24 +0100

    Day 8

---
+++ 2023/day8/day8.go
@@ -0,0 +1,120 @@
+package day8
+
+import (
+	"bufio"
+	"github.com/mariamihai/advent-of-code/util"
+	"strings"
+)
+
+func Problem1(filename string) int {
+	isEndNodeCondition := func(currentNode Node) bool { return currentNode.value == "ZZZ" }
+
+	instructions, network := getInput(filename)
+
+	return calculationForValue(network, instructions, network["AAA"], isEndNodeCondition)
+}
+
+func Problem2(filename string) int {
+	isStartNodeCondition := func(node Node) bool { return string(node.value[len(node.value)-1:]) == "A" }
+	isEndNodeCondition := func(currentNode Node) bool { return string(currentNode.value[len(currentNode.value)-1:]) == "Z" }
+
+	instructions, network := getInput(filename)
+	pathsResults := []int{}
+
+	for _, node := range network {
+		if isStartNodeCondition(node) {
+			pathsResults = append(pathsResults, calculationForValue(network, instructions, node, isEndNodeCondition))
+		}
+	}
+
+	return multiple(pathsResults...)
+}
+
+func calculationForValue(network map[string]Node, instructions string, node Node, endNodeCondition func(Node) bool) int {
+	result := 0
+	step := 0
+
+	getNextNode := func(stepInstruction uint8) Node {
+		if stepInstruction == 'L' {
+			return network[node.left]
+
+		} else {
+			return network[node.right]
+		}
+	}
+
+	for true {
+		if endNodeCondition(node) {
+			break
+		}
+
+		node = getNextNode(instructions[step%len(instructions)])
+
+		result++
+		step++
+	}
+
+	return result
+}
+
+// divisor - GDC / CMMDC
+func divisor(nr1, nr2 int) int {
+	for nr2 != 0 {
+		temp := nr2
+		nr2 = nr1 % nr2
+		nr1 = temp
+	}
+
+	return nr1
+}
+
+// multiple - LCM / CMMMC
+func multiple(nrs ...int) int {
+	result := nrs[0] * nrs[1] / divisor(nrs[0], nrs[1])
+
+	for i := 2; i < len(nrs); i++ {
+		result = multiple(result, nrs[i])
+	}
+
+	return result
+}
+
+type Node struct {
+	value       string
+	left, right string
+}
+
+func getInput(filename string) (string, map[string]Node) {
+	file := util.ReadFile(filename)
+	defer util.CloseFile()(file)
+
+	network := make(map[string]Node)
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	instructions := scanner.Text()
+
+	scanner.Scan()
+	scanner.Text()
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.Replace(line, " ", "", -1)
+		line = strings.Replace(line, "(", "", -1)
+		line = strings.Replace(line, ")", "", -1)
+
+		value := strings.Split(line, "=")[0]
+		leftRight := strings.Split(line, "=")[1]
+		leftNode := strings.Split(leftRight, ",")[0]
+		rightNode := strings.Split(leftRight, ",")[1]
+
+		network[value] = Node{
+			value: value,
+			left:  leftNode,
+			right: rightNode,
+		}
+	}
+
+	return instructions, network
+}
